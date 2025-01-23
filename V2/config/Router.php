d8303e98e8954bf4afcdec9da95d4d8d00a5b0d7
Sabrina AEH <sabrina.aeh84@gmail.com>
2025-01-23 09:28:27 +0000

    step 1 Router done

---
+++ V2/config/Router.php
@@ -0,0 +1,31 @@
+
+<?php
+//require_once './controllers/UserController.php';
+
+class Router {
+    public function __construct() 
+    {
+       
+    }
+
+    public function handleRequest(array $get): void 
+    {
+        
+        if (isset($get["route"]) && $get["route"] === "show_user") {
+            // $userController->show();
+        } elseif (isset($get["route"]) && $get["route"] === "create_user") {
+            // $userController->create();
+        } elseif (isset($get["route"]) && $get["route"] === "check_create_user") {
+            // $userController->checkCreate();
+        } elseif (isset($get["route"]) && $get["route"] === "update_user") {
+            // $userController->update();
+        } elseif (isset($get["route"]) && $get["route"] === "check_update_user") {
+            // $userController->checkUpdate();
+        } elseif (isset($get["route"]) && $get["route"] === "delete_user") {
+            // $userController->delete();
+        } else {
+            // $userController->list();
+        }
+    }
+}
+?>
